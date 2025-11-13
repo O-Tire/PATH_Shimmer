29d900c3ce8f4149aaaa37fea23cedc3b239d8e1
O-Tire <164386722+O-Tire@users.noreply.github.com>
2025-11-13 13:05:57 +0330

    v1.3.0
    Switched to .NET 10. The app is now self-contained. Added .gitignore.

--- redirector/obj/Debug/net9.0/redirector.AssemblyInfo.cs
+++ redirector/obj/Debug/net9.0/redirector.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("redirector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4297b4612222fb1ede742fe50111b245b599215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be1bd55aedfa8fc8d8d43ddc0996dc1c5ee76e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("redirector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("redirector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- redirector/obj/Release/net9.0/redirector.AssemblyInfo.cs
+++ redirector/obj/Release/net9.0/redirector.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("redirector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97d00cf1f40c9b4355f238f5b003c0c22ee1a6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be1bd55aedfa8fc8d8d43ddc0996dc1c5ee76e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("redirector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("redirector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
